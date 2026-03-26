764e09580413a6773fff7740a0651499b5246189
jabushpenka <daskovartem05@gmail.com>
2026-03-26 16:36:32 +0700

    Update mydb.sql
    renamed "user" and "board" tables to "users" and "boards" because "user" conflicted PostgreSQL syntax

--- db/mydb.sql
+++ db/mydb.sql
@@ -1,4 +1,8 @@
-CREATE TABLE IF NOT EXISTS "user" (
+DROP TABLE IF EXISTS public."user"
+DROP TABLE IF EXISTS public."board"
+DROP TABLE IF EXISTS public."link"
+
+CREATE TABLE IF NOT EXISTS "users" (
     "user_id"   SERIAL NOT NULL,
 	"name"  varchar(40) NOT NULL UNIQUE,
 	"password"	varchar(40) NOT NULL,
@@ -7,11 +11,12 @@ CREATE TABLE IF NOT EXISTS "user" (
 	PRIMARY KEY("user_id")
 );
 
-CREATE TABLE IF NOT EXISTS "board" (
+CREATE TABLE IF NOT EXISTS "boards" (
 	"board_id"	SERIAL NOT NULL,
 	"name"	varchar(40) NOT NULL,
 	"address"	varchar(10) NOT NULL UNIQUE,
 	"about"	TEXT,
+	"contents" jsonb,
 	PRIMARY KEY("board_id")
 );
 
@@ -19,6 +24,6 @@ CREATE TABLE IF NOT EXISTS "link" (
 	"user_id"	INTEGER NOT NULL,
 	"board_id"	INTEGER NOT NULL,
 	PRIMARY KEY("user_id","board_id"),
-	CONSTRAINT "K2" FOREIGN KEY("board_id") REFERENCES "board"("board_id"),
-	CONSTRAINT "K1" FOREIGN KEY("user_id") REFERENCES "user"("user_id")
+	CONSTRAINT "K2" FOREIGN KEY("board_id") REFERENCES "boards"("board_id"),
+	CONSTRAINT "K1" FOREIGN KEY("user_id") REFERENCES "users"("user_id")
 );
