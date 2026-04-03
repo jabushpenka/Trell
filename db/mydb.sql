83f9065e775d288a9a0dd57d6a1dfb0bc4bc9b37
jabushpenka <daskovartem05@gmail.com>
2026-04-03 22:11:56 +0700

    Update mydb.sql
    изменены имена чтобы не конфликтовать с внутрянкой PostgreSQL
    добавлена таблица ролей пользователей, для регулирования полномочий
    изменены некоторые типы данных

--- db/mydb.sql
+++ db/mydb.sql
@@ -1,11 +1,13 @@
-DROP TABLE IF EXISTS public."user";
-DROP TABLE IF EXISTS public."board";
-DROP TABLE IF EXISTS public."link";
+DROP TABLE IF EXISTS "links";
+DROP TABLE IF EXISTS "link";
+DROP TABLE IF EXISTS "users";
+DROP TABLE IF EXISTS "boards";
+DROP TABLE IF EXISTS "roles";
 
 CREATE TABLE IF NOT EXISTS "users" (
     "user_id"   SERIAL NOT NULL,
-	"name"  varchar(40) NOT NULL UNIQUE,
-	"password"	TEXT NOT NULL,
+	"user_name"  varchar(40) NOT NULL UNIQUE,
+	"pword"	VARCHAR(100) NOT NULL,
 	"email"	varchar(40) NOT NULL UNIQUE,
 	"photo"	varchar(40),
 	PRIMARY KEY("user_id")
@@ -13,17 +15,32 @@ CREATE TABLE IF NOT EXISTS "users" (
 
 CREATE TABLE IF NOT EXISTS "boards" (
 	"board_id"	SERIAL NOT NULL,
-	"name"	varchar(40) NOT NULL,
-	"address"	varchar(10) NOT NULL UNIQUE,
+	"board_name"	varchar(40) NOT NULL,
+	"address"	varchar(40) NOT NULL UNIQUE,
 	"about"	TEXT,
 	"contents" jsonb,
 	PRIMARY KEY("board_id")
 );
 
-CREATE TABLE IF NOT EXISTS "link" (
+CREATE TABLE IF NOT EXISTS "roles" (
+	"role_id"	SERIAL NOT NULL,
+	"role_name"	VARCHAR(40) NOT NULL,
+	"description"	TEXT,
+	PRIMARY KEY("role_id")
+);
+
+CREATE TABLE IF NOT EXISTS "links" (
 	"user_id"	INTEGER NOT NULL,
 	"board_id"	INTEGER NOT NULL,
+	"role_id"   INTEGER NOT NULL DEFAULT 4,
 	PRIMARY KEY("user_id","board_id"),
-	CONSTRAINT "K2" FOREIGN KEY("board_id") REFERENCES "boards"("board_id"),
-	CONSTRAINT "K1" FOREIGN KEY("user_id") REFERENCES "users"("user_id")
+	CONSTRAINT "K1" FOREIGN KEY("user_id") REFERENCES "users"("user_id") ON DELETE CASCADE,
+	CONSTRAINT "K2" FOREIGN KEY("board_id") REFERENCES "boards"("board_id") ON DELETE CASCADE,
+	CONSTRAINT "K3" FOREIGN KEY("role_id") REFERENCES "roles"("role_id") ON DELETE SET DEFAULT
 );
+
+INSERT INTO roles ("role_name","description") VALUES
+    ('Owner','can manage the entire board'),
+    ('Administrator','can manage columns'),
+	('Manager','can manage cards'),
+	('Worker','can manage tasks');
